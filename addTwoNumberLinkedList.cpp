d315b13f5ec4bb6621d8987f33ad4768b96836fc
abajpai14 <55253291+abajpai14@users.noreply.github.com>
2019-09-12 17:32:58 -0700

    Create addTwoNumberLinkedList.cpp
    problem:
    given two linkedlist (revers is a number)
    add both the number and return result in linked list.
    
    Input: (4 ->1) + (3->1)
    Output: 7 -> 2
    14+13 = 27

---
+++ addTwoNumberLinkedList.cpp
@@ -0,0 +1,27 @@
+
+
+Node* addTwoNumbers(Node* number1, Node* number2) {
+        int num1 = 0;
+        int num2 = 0; 
+        int carry = 0;
+        int total  = 0;
+        Node* finalNumber = new Node(0);
+        Node* result = finalNumber;
+       while(number1!=NULL || number2!=NULL)
+       {
+           num1 = (number1!=NULL)?number1->val : 0;
+           num2 = (number2!=NULL) ?number2->val: 0;
+           total = carry + num1 + num2;
+           carry = total/10;
+           finalNumber->next = new Node(total %10);
+           finalNumber = finalNumber->next;
+           if (number1)
+           number1 = number1->next;
+           if (number2)
+           number2 =  number2->next;
+       }
+        
+        if(carry)
+            finalNumber->next = new Node(carry);
+        return result->next;
+}
